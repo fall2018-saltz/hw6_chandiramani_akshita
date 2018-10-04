a095e0acfa5692283f33d7d3c2a5c694243e7095
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:37:14 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,6 +1,6 @@
 
 #1)	Read in the census dataset (using the function created in HW 3)
-head(states)
+head(raw_data)
 CleanDataframe <- function()
 {
   dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"),stringsAsFactors = FALSE)
