4ebf237b8e03a70596139980be74cff89bd97f38
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 11:30:27 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,3 +1,23 @@
 
-clean_data <- raw_data
-head(clean_data)
+#1)	Read in the census dataset (using the function created in HW 3)
+head(states)
+CleanDataframe <- function()
+{
+  dfStates <- read.csv(url("https://www2.census.gov/programs-surveys/popest/datasets/2010-2017/state/asrh/scprc-est2017-18+pop-res.csv"),stringsAsFactors = FALSE)
+  
+  #Removing Puerto Rico and the summary for the united states (first row)
+  dfStates_reqRows <- dfStates[-c(1,nrow(dfStates)),]
+  
+  # Keeping only the required col names (stateName, population, popOver18, percentOver18) 
+  # and removing the rest 1st, 2nd, 3rd and 4th column
+  dfStates_reqCols <- dfStates_reqRows[,-c(1,2,3,4)]
+  
+  cleanDfStates <- dfStates_reqCols
+  return(cleanDfStates)
+}
+
+cleanDfStates <- CleanDataframe()
+
+#Initializing arrests with USArrests data
+arrests <- USArrests
+head(arrests)
