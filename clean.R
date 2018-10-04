cf2b48430461bc5db59c042f4be2772fb9b26223
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:14:01 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -16,7 +16,7 @@ CleanDataframe <- function(states)
 }
 
 cleanDfStates <- CleanDataframe(raw_data)
-rownames(cleanDfStates)
+rownames(arrests)
 head(cleanDfStates)
 
 #Initializing arrests with USArrests data
