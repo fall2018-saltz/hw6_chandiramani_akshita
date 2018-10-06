6397262ff3736401360a51323b5750046396f61b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 12:56:24 -0400

    Saving ScatterPlot.R

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -1,4 +1,4 @@
 
 library(ggplot2)
 head(mergedData)
-scatterPlot<-ggplot(mergedData , aes( x=population, y=percentOver18,color=density)) + geom_point()
+scatterPlot<-ggplot(mergedData , aes( x=population, y=percentOver18,color=Murder)) + geom_point()
