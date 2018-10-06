e792343553cefe076f88c5f28a05d9b343db8154
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 10:55:22 -0400

    Saving Plots.R

--- Plots.R
+++ Plots.R
@@ -2,4 +2,4 @@
 library(ggplot2)
 mergedDataDF<-na.omit(mergedData)
 
-ggplot(mergedDataDF, aes(x=mergedDataDF$population)) + geom_histogram()
+ggplot(mergedDataDF, aes(x=mergedDataDF$population), bins=30) + geom_histogram()
