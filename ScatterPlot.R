f8217d0252e84c80f6be963363381aa4fcc0a002
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 12:49:31 -0400

    Saving ScatterPlot.R

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -1,4 +1,4 @@
 
 library(ggplot2)
 head(mergedData)
-ggplot(mergedData , aes(x=population, y=percentOver18)) + geom_point()
+scatterPlot<-ggplot(mergedData , aes(x=population, y=percentOver18)) + geom_point()
