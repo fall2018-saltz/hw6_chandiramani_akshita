123d94216d7f2c5572931c230f22a040cad9e15a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 11:06:17 -0400

    Saving Plots.R

--- Plots.R
+++ Plots.R
@@ -3,4 +3,7 @@ str(mergedData)
 library("ggplot2")
 populationPlot<-ggplot(mergedData, aes(x=population)) + 
   geom_histogram(binwidth=5000000, color="black", fill="white")
+  
+murderPlot<-ggplot(mergedData, aes(x=Murder)) + 
+  geom_histogram(binwidth=5000000, color="black", fill="white")
 
