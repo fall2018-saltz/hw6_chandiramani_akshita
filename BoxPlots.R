b1f9a84e61cf22da178719f6cbb546dbfff032b2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 11:37:40 -0400

    Saving BoxPlots.R

--- BoxPlots.R
+++ BoxPlots.R
@@ -3,15 +3,7 @@ library("ggplot2")
 populationBoxPlot<-ggplot(mergedData, aes(x="",y=population)) + 
   geom_boxplot(color="black", fill="white")
   
-murderPlot<-ggplot(mergedData, aes(x=Murder)) + 
-  geom_histogram(color="black", fill="white")
+murderBoxPlot<-ggplot(mergedData, aes(x="",y=Murder)) + 
+  geom_boxplot(color="black", fill="white")
   
-assaultPlot<-ggplot(mergedData, aes(x=Assault)) + 
-  geom_histogram(binwidth=50, color="black", fill="white")
   
-urbanPopPlot<-ggplot(mergedData, aes(x=UrbanPop)) + 
-  geom_histogram(binwidth=10, color="black", fill="white")
-  
-rapePlot<-ggplot(mergedData, aes(x=Rape)) + 
-  geom_histogram(binwidth=5, color="black", fill="white") 
-
