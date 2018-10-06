d0c623001c25e1f7a33c6bbe200fa583533fcbcd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 11:53:30 -0400

    Saving mostMurders.R

--- mostMurders.R
+++ mostMurders.R
@@ -4,3 +4,4 @@ head(mergedData)
 mergedData$MurderNumbers <- (mergedData$Murder * mergedData$population)/100000
 head(mergedData)
 
+mostMurders<-ggplot() + geom_col(data = mergedData, aes(x = StateName, y = MurderNumbers))
