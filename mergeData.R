29ce0fa17dfdeab7bd9b14756f220719376032f6
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-06 10:28:23 -0400

    Saving mergeData.R

--- mergeData.R
+++ mergeData.R
@@ -1,7 +1,7 @@
 
-
+nrow(arrests)
 mergedData <- merge(arrests, cleanDfStates, by="row.names", all=TRUE)
-head(mergedData,15)
+
 
 mergedData <- mergedData[,-c(6)]
 colnames(mergedData)[1] <- "StateName"
