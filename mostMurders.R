a8f30dcd383356ea190a30d7b0ac04802c064427
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 11:02:37 -0400

    Saving mostMurders.R

--- mostMurders.R
+++ mostMurders.R
@@ -8,6 +8,8 @@ mostMurders<- ggplot(mergedData, title="mostMurders") +
   geom_col(aes(x = StateName, y = MurderNumbers))+labs(x="Total Murders" )+ 
   theme(axis.text.x = element_text(angle=45))
   
+  mostMurders
+  
 sortedMostMurders <- ggplot(mergedData, title="mostMurders") + 
   geom_col(aes( x = reorder(StateName, -MurderNumbers), y = MurderNumbers))+labs(x="Total Murders" )+ 
   theme(axis.text.x = element_text(angle=45))
