a89548284d4cad80f61b34b21c342107c7a3a6ff
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:30:02 -0400

    Saving HistogramPlots.R

--- HistogramPlots.R
+++ HistogramPlots.R
@@ -3,23 +3,24 @@
 
 library("ggplot2")
 
-#Create histogram for population on the x-axis and add attributes like color and fill
+#4) Create histogram for population on the x-axis and add attributes like color and fill
 populationPlot<-ggplot(mergedData, aes(x=population)) + 
   geom_histogram(color="black", fill="white")
 
-#Create similar histogram for murder  
+#Create similar histogram for murder 
 murderPlot<-ggplot(mergedData, aes(x=Murder)) + 
   geom_histogram(color="black", fill="white")
   
- #Create similar histogram for assault 
+ #Create similar histogram for assault (setting bin width to 5)
 assaultPlot<-ggplot(mergedData, aes(x=Assault)) + 
   geom_histogram(binwidth=50, color="black", fill="white")
   
- #Create histogram for urbanPop
+ #Create histogram for urbanPop (setting bin width to 5)
 urbanPopPlot<-ggplot(mergedData, aes(x=UrbanPop)) + 
   geom_histogram(binwidth=10, color="black", fill="white")
   
-  #Create histogram for rape
+  #Create histogram for rape (setting bin width to 5)
 rapePlot<-ggplot(mergedData, aes(x=Rape)) + 
   geom_histogram(binwidth=5, color="black", fill="white")
 
+#To make the other histograms look right, we either adjust the bin width (range of each bin) 
