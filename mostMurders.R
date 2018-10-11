14fd35aea89926287e02e5a2acfa15aac5407700
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:41:55 -0400

    Saving mostMurders.R

--- mostMurders.R
+++ mostMurders.R
@@ -2,15 +2,16 @@
 #Use the ggplot2 library
 library(ggplot2)
 
-#Calculate number of murders per state
+#7) Calculate number of murders per state
 mergedData$MurderNumbers <- round((mergedData$Murder * mergedData$population)/100000)
 mergedData$StateName <- as.factor(mergedData$StateName)
 head(mergedData)
 
+#8) Generate a bar chart, with the number of murders per state
 mostMurders<- ggplot(mergedData, title="mostMurders") + 
   geom_col(aes(x = StateName, y = MurderNumbers))+labs(x="Total Murders" )+ 
   theme(axis.text.x = element_text(angle=45))
-  
+ 
   mostMurders
   
 sortedMostMurders <- ggplot(mergedData, title="mostMurders") + 
