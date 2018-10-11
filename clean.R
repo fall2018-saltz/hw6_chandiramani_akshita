5ce61a86c48829051fcf3197c18f99d66ad6ef89
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:18:46 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -9,7 +9,7 @@ CleanDataframe <- function(states)
   
   # Keeping only the required col names (stateName, population, popOver18, percentOver18) 
   # and removing the rest 1st, 2nd, 3rd and 4th column
-  dfStates_reqCols <- dfStates_reqRows[,-c(1,2,3,4)]
+  dfStates_reqCols <- dfStates_reqRows[,-c(1,2,3,4) ]
   colnames(dfStates_reqCols)<-c("stateName","population","popOver18","percentOver18")
   cleanDfStates <- dfStates_reqCols
   return(cleanDfStates)
