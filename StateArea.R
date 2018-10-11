5f4ba618c38f0767ca9915f3fb83c5c2d94285fa
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 11:29:53 -0400

    Saving StateArea.R

--- StateArea.R
+++ StateArea.R
@@ -1,2 +1,2 @@
 
-#the area of each state, and the center of each state, to the merged dataframe, using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
+#the area of eachstate, and the center of each state, to the merged dataframe, using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
