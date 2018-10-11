5359964bea3af5c9c8faddb60bca5d7dc7607316
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:36:53 -0400

    Saving BoxPlots.R

--- BoxPlots.R
+++ BoxPlots.R
@@ -3,6 +3,7 @@ library("ggplot2")
 
 #5)	Create a boxplot for the population, and a different boxplot for the murder rate.
 #A single box plot is created to display the distribution and the x-axis is blank since the population
+#is not divided by any factor such as state.
 populationBoxPlot<-ggplot(mergedData, aes(x="",y=population)) + 
   geom_boxplot(color="black", fill="white")
   
