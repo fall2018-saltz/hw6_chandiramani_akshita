bc66733a867dcd79f01ffe807206e80e9bf3cd82
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 22:23:12 -0400

    Saving mergeData.R

--- mergeData.R
+++ mergeData.R
@@ -3,6 +3,7 @@
 mergedData <- merge(arrests, cleanDfStates, by="row.names", all=TRUE)
 
 #Remove additional States column from merged table
+#and rename to StateName
 mergedData <- mergedData[,-c(6)]
 colnames(mergedData)[1] <- "StateName"
 
